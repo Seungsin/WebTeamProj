c222cf5fa6d504ccfa7447b73160e63fbc38af8b
Seungsin <8050aa@naver.com>
2021-06-07 23:16:14 +0900

    modified commits all

--- controllers/shCommentStore.js
+++ controllers/shCommentStore.js
@@ -1,10 +1,12 @@
 const Share = require('../models/Share.js')
+const Name = require('../models/User')
 
 module.exports = async (req, res)=>{
     const post = req.params.id
     const comment = {
         content: req.body.content,
         userid: req.session.userId,
+        username: name.username,
     }
     await Share.findByIdAndUpdate(post, {$addToSet:{"comments" : comment}}, function(err, docs){
 
