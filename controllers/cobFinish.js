4f8c0d314132b2b807392502070854dcd4da5196
Seungsin <8050aa@naver.com>
2021-06-08 01:41:04 +0900

    update

--- controllers/cobFinish.js
+++ controllers/cobFinish.js
@@ -2,5 +2,5 @@ const Cobuying = require('../models/Cobuying.js')
 
 module.exports = async (req, res) =>{
     await Cobuying.findByIdAndUpdate(req.params.id, {"isfinished":1}, function(err, docs){})
-    res.redirect('/cobuyingpost/'+req.params.id)
+    res.redirect('/cobuying')
 }
