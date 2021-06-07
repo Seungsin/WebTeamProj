d3bd8c6ece3d682a4bf917a016674211dacf993c
Seungsin <8050aa@naver.com>
2021-06-07 22:45:28 +0900

    modified

--- controllers/exchangeStore.js
+++ controllers/exchangeStore.js
@@ -1,27 +1,27 @@
 const Exchange = require('../models/Exchange.js')
 const path = require('path')
 
-module.exports = (req, res)=>{
+module.exports = async (req, res)=>{
     let image={};
     if(!req.files){
         image.name="baisic.jpg"
-        await Cobuying.create({
+        await Exchange.create({
             ...req.body,
             image: '/assets/img/'+image.name,
             userid: req.session.userId
         })
         
-        res.redirect('/cobuying')
+        res.redirect('/exchange')
     }else{
         image=req.files.image
         image.mv(path.resolve(__dirname, '..','public/assets/img', image.name), async (error)=>{
-            await Cobuying.create({
+            await Excahnge.create({
                 ...req.body,
                 image: '/assets/img/'+image.name,
                 userid: req.session.userId
             })
             
-            res.redirect('/cobuying')
+            res.redirect('/exchange')
         })
     }
 }
