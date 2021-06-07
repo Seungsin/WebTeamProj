826d64d1a6618e35dacfb693d9d4786e0fb5f9c7
Seungsin <8050aa@naver.com>
2021-06-08 01:10:19 +0900

    for owner

--- controllers/cobuyingGet.js
+++ controllers/cobuyingGet.js
@@ -2,9 +2,10 @@ const Cobuying = require('../models/Cobuying.js')
 
 module.exports = async (req, res) =>{
     const post = await (await Cobuying.findById(req.params.id))
-    const isOwner = 0
+    let isOwner = 0
     if(post.userid == req.session.userId) isOwner=1
+    post.isOwner=isOwner
     res.render('cobuyingpost', {
-        post, isOwner
+        post
     })
 }
--- controllers/exchangeGet.js
+++ controllers/exchangeGet.js
@@ -2,7 +2,9 @@ const Exchange = require('../models/Exchange.js')
 
 module.exports = async (req, res) =>{
     const post = await(await Exchange.findById(req.params.id))
-    console.log(post)
+    let isOwner = 0
+    if(post.userid == req.session.userId) isOwner=1
+    post.isOwner=isOwner
     res.render('exchangepost', {
         post
     })
