dfbfc6f7ddd313b7aed88182ddfad2e49a9c9f14
Seungsin <8050aa@naver.com>
2021-06-08 00:45:49 +0900

    new update

--- controllers/exchangeGet.js
+++ controllers/exchangeGet.js
@@ -1,7 +1,8 @@
 const Exchange = require('../models/Exchange.js')
 
 module.exports = async (req, res) =>{
-    const post = await (await Exchange.findById(req.params.id))
+    const post = await(await Exchange.findById(req.params.id))
+    console.log(post)
     res.render('exchangepost', {
         post
     })
