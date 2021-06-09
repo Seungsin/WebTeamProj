d98e5fbf0e5656f3a921bcc521f6e7a3f8f75237
Seungsin <8050aa@naver.com>
2021-06-09 23:51:36 +0900

    resolve photo error

--- controllers/exchangeStore.js
+++ controllers/exchangeStore.js
@@ -15,7 +15,7 @@ module.exports = async (req, res)=>{
     }else{
         image=req.files.image
         image.mv(path.resolve(__dirname, '..','public/assets/img', image.name), async (error)=>{
-            await Excahnge.create({
+            await Exchange.create({
                 ...req.body,
                 image: '/assets/img/'+image.name,
                 userid: req.session.userId
